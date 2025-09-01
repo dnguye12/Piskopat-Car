8c9b3b12f6b2f8e2b68e6517a1a26bdb2c6ecf74
dnguye12 <duchuyng051@gmail.com>
2025-09-01 12:40:57 +0200

    reset

--- src/routes/contact/components/ContactForm.tsx
+++ src/routes/contact/components/ContactForm.tsx
@@ -56,6 +56,7 @@ export function ContactForm() {
                 body: JSON.stringify(body),
             })
 
+            form.reset()
             setSending(false)
         } catch (e) {
             console.log(e)
